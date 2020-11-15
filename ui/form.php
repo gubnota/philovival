afcec1eacedfcc27cbd4030a662136db3f998742
马拉迪 Larry Moore <wx_a5f8999299b24748a127c3436eb5426b@git.code.tencent.com>
2020-11-15 17:46:12 +0800

    ui translations

---
+++ ui/form.php
@@ -0,0 +1,29 @@
+<?php
+$d = __DIR__;
+$k =["en.txt","es.txt","hi.txt","id.txt","jp.txt","pt.txt","ru.txt","tr.txt","vi.txt","zh.txt"];
+$b = "en.txt";
+$g = file("$d/$b");
+foreach($k as $a){
+$fa = file("$d/$a");
+$out = '{"contents":[';
+for($i=0; $i<count($g); $i++){
+// echo "k: \"$($g[$i])\" v: \"{$fa[$i]}\", lang:$a\n";
+//'{"k": "OK","v": "Согласен"},';
+$out .= '{"k": "';
+$out .= rtrim($g[$i]);
+$out .= '", "v": "';
+$out .= rtrim($fa[$i]);
+$out .= '"},';
+}
+$out = substr($out,0, strlen($out)-1);
+$out .= ']}';
+
+$file_name = "$d/";
+$file_name .= substr($a,0,2);
+$file_name .= ".json";
+// echo "$file_name\n\n";
+// echo $out;
+// echo "\n\n";
+file_put_contents($file_name, $out);
+}
+?>
